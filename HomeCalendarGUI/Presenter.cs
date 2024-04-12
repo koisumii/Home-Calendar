104e06796bd1330774da3cdcd0fe5e0ae0f07940
Alexander Burlec-Plaies <2162243@johnabbottcollege.net>
2024-04-12 11:24:37 -0400

    Created Presenter.

---
+++ HomeCalendarGUI/Presenter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calendar;
+
+namespace HomeCalendarGUI
+{
+    internal class Presenter
+    {
+        private readonly HomeCalendar model;
+        private IView view;
+
+        public Presenter(IView v) 
+        {
+            model = new HomeCalendar();
+            view = v;
+        }
+
+    }
+}
