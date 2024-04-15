6b967e4d6cc1836cda0ea69cfc977d429803602d
LauraaMolinaa <2232607@johnabbottcollege.net>
2024-04-15 12:10:32 -0400

    pretty much done with the new category window

--- HomeCalendarGUI/MainWindow.xaml.cs
+++ HomeCalendarGUI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Calendar;
+using System.Data.SQLite;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static Calendar.Category;
 
 namespace HomeCalendarGUI
 {
@@ -16,9 +19,65 @@ namespace HomeCalendarGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Presenter _presenter; 
         public MainWindow()
         {
             InitializeComponent();
+            ShowEventsTypeOnCmb();
+            _presenter = new Presenter();
+        }
+
+        public void ShowEventsTypeOnCmb()
+        {
+            cmbEventTypes.Items.Add(Category.CategoryType.Event);
+            cmbEventTypes.Items.Add(Category.CategoryType.AllDayEvent);
+            cmbEventTypes.Items.Add(Category.CategoryType.Holiday);
+            cmbEventTypes.Items.Add(Category.CategoryType.Availability);
+        }
+
+
+        private void Button_ClickAddCategory(object sender, RoutedEventArgs e)
+        {
+            var eventTypeChoice = cmbEventTypes.SelectedItem;
+            string desc = DescriptionBox.Text;
+
+            if (eventTypeChoice != null || desc != null) 
+            {
+                CategoryType type = (CategoryType)eventTypeChoice;
+                _presenter.AddNewCategory(desc, type);
+                DisplaySuccessfulMessage("Category has been successfully added!");
+            }
+            else
+            {
+                DisplayErrorMessage("You can not leave any empty boxes."); 
+            }
+            
+        }
+
+        public Category.CategoryType GetEnumFromString(string str)
+        {
+            foreach (CategoryType types in Enum.GetValues(typeof(Category.CategoryType))) 
+            { 
+                if(types.ToString() == str)
+                {
+                    return types; 
+                }
+            }
+
+            // safety net 
+            return CategoryType.Event; 
+        }
+
+        public void DisplayErrorMessage(string msg)
+        {
+            message.Foreground = Brushes.Red;
+            message.Text = msg;
+        }
+
+        public void DisplaySuccessfulMessage(string msg)
+        {
+            message.Foreground = Brushes.Green;
+            message.Text = msg; 
         }
     }
 }
--- HomeCalendarGUI/Presenter.cs
+++ HomeCalendarGUI/Presenter.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Calendar;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations.Model;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
+using TeamHeavyWeight_HomeCalendarApp;
+using static Calendar.Category;
 
 namespace HomeCalendarGUI
 {
     internal class Presenter
     {
+        private HomeCalendar _model; 
+        public Presenter()
+        {
+            _model = new HomeCalendar();
+        }
+
+        public void AddNewCategory(string desc, CategoryType type = CategoryType.Event)
+        {
+            _model.categories.Add(desc, type);
+        }
     }
 }
