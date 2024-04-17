687e9d232f34e5dcab534989fae71caba7bd60a4
LauraaMolinaa <2232607@johnabbottcollege.net>
2024-04-17 12:50:06 -0400

    drop down box for categories done but still have to test it with th rest of the window

---
+++ HomeCalendarGUI/Events.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Calendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HomeCalendarGUI
+{
+    /// <summary>
+    /// Interaction logic for Events.xaml
+    /// </summary>
+    public partial class Events : Window
+    {
+        public Events()
+        {
+            InitializeComponent();
+            ShowCategoriesOnCmb();
+        }
+
+        private void cmbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        public void ShowCategoriesOnCmb()
+        {
+            HomeCalendar h1 = new HomeCalendar();
+            List<Category> categories = h1.categories.List();
+
+            for (int i = 0; i < categories.Count; i++)
+            {
+                cmbCategory.Items.Add(categories[i]);
+            }
+        }
+    }
+}
