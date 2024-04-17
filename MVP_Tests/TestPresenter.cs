a0f375206993f7132baff72d38703de7f537d219
LauraaMolinaa <2232607@johnabbottcollege.net>
2024-04-17 13:49:48 -0400

    added dll on test project

---
+++ MVP_Tests/TestPresenter.cs
@@ -0,0 +1,12 @@
+using HomeCalendarGUI;
+namespace MVP_Tests
+{
+    public class TestPresenter
+    {
+        [Fact]
+        public void TestAddNewCategory_Fail()
+        {
+            Presenter p = new Presenter();
+        }
+    }
+}
